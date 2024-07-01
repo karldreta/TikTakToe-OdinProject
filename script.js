94af6346e9da6a5f53ed623c72ce872033799fdc
karldreta <karldreta@gmail.com>
2024-07-02 01:58:09 +0800

    Initialize playGame

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// const square = document.querySelectorAll('.square');
-// square.forEach(square => square.addEventListener('click', placeMarker));
 
-function gameBoardObj () {
-    return ['', '', '', '', '', '', '', '', '',];
-};
-                
+function gameBoardObj() {
+    const gameBoard = {
+        board: ['', '', '', '', '', '', '', '', ''],
+    };
+
+    return {gameBoard}; // Note: You are returning an object, so when you invoke this function you will still have to dive deeper to get the array.
+}
+
+// Below: a factory for players
 function createPlayer(name, marker) {
+    // In here, you are returning a function which means you still have to invoke that function to get its return values (i.e. playerName, playerMarker).
     return function () {
         return { 
             playerName: name,
@@ -15,9 +19,39 @@ function createPlayer(name, marker) {
 }
 
 function playGame () {
-    const board = gameBoardObj();
+    const board = gameBoardObj().gameBoard.board;
+    const player1 = createPlayer("Karl", "X")();
+    const player2 = createPlayer("James", "O")();
+    let currentPlayer = player1;
 
-    return board
+    const square = document.querySelectorAll('.square');
+    square.forEach(square => square.addEventListener('click', () => {
+        const index = square.getAttribute('data-index');
+        // console.log(currentPlayer);
+        if (currentPlayer === player1) {
+            if (board[index] === '') {
+                board[index] = currentPlayer.playerMarker;
+                currentPlayer = player2;
+            } else {
+                console.log("Not empty");
+            }
+        } else {
+            if (board[index] === '') {
+            board[index] = currentPlayer.playerMarker;
+            currentPlayer = player1;
+            }
+            else {
+                console.log("Not empty");
+            }
+        }
+        console.log(board); // I need to return this
+        return board
+    }));
+
 };
 
-console.log(playGame());
+function checkWinner () {
+    return 
+}
+
+playGame();
